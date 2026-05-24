0e9122ddcee4c31ca86c406b067aee5116ad2d85
5450L <susalafamilia@gmail.com>
2026-05-24 15:32:26 +0300

    Refactor cookie token extraction to use a regular expression for improved parsing

--- server/src/utils/cookies.ts
+++ server/src/utils/cookies.ts
@@ -1,4 +1,4 @@
 export const getTokenFromCookie = (cookieHeader: string | undefined): string | undefined => {
   if (!cookieHeader) return undefined;
-  return cookieHeader.split('; ').find(cookie => cookie.startsWith('token='))?.split('=')[1];
+  return cookieHeader.split(/;\s*/).find(cookie => cookie.startsWith('token='))?.split('=')[1];
 };
