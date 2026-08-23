ce4f00d1a4539aac86eaa315eb4f9e1051640e3d
5450L <susalafamilia@gmail.com>
2026-08-23 14:47:38 +0300

    Set keep-alive and headers timeout for HTTP server

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,13 @@ import { config } from './config';
 import { startDemoReseed } from './services/demoReseed';
 import { initSocket } from './services/socket';
 
+const ALB_IDLE_TIMEOUT_MS = 60_000;
+const KEEP_ALIVE_MARGIN_MS = 5_000;
+
 const httpServer = createServer(createApp());
+
+httpServer.keepAliveTimeout = ALB_IDLE_TIMEOUT_MS + KEEP_ALIVE_MARGIN_MS;
+httpServer.headersTimeout = ALB_IDLE_TIMEOUT_MS + KEEP_ALIVE_MARGIN_MS * 2;
 
 initSocket(httpServer);
 startDemoReseed();
